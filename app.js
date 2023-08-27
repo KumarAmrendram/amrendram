e6ebc3658830f39df1564c496b24c6b1bd0234b2
kumarAmrendram <amrendram07@gmail.com>
2023-08-27 16:07:27 +0530

    - initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+const blogData = require("./public/db/blogData.json");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.get("/", (req, res) => {
+    res.render("index", { blogs: blogData });
+});
+
+app.get("/blog/:blogName", (req, res) => {
+    let flag = 1;
+    for (let i = 0; i < blogData.length; i++) {
+        if (blogData[i].name === req.params.blogName) {
+            flag = 1;
+            res.status(200).render("blog", { blog: blogData[i] });
+            //   res.status(200).send({ blogData });
+            return;
+        } else {
+            flag = 0;
+        }
+    }
+    if (flag === 0) {
+        res.send("Error 404: Not Found ");
+    }
+});
+
+const PORT = 7000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+let sidebar = document.getElementById("sidebar");
+function showAbout() {
+  if (sidebar.classList.contains("hide-about-area")) {
+    sidebar.classList.remove("hide-about-area");
+  } else {
+    sidebar.classList.add("hide-about-area");
+  }
+}
+
+
+let mobileNav = document.querySelector(".mobile-nav");
+function showNavbar() {
+  if (mobileNav.classList.contains("hide-mobi-nav")) {
+    mobileNav.classList.remove("hide-mobi-nav");
+  } else {
+    mobileNav.classList.add("hide-mobi-nav");
+  }
+}
