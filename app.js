9217431fca5d2e213be3d959c9dcd208757c3c20
kumarAmrendram <amrendram07@gmail.com>
2023-08-28 15:38:43 +0530

    - final app.js and index.js

--- app.js
+++ app.js
@@ -6,34 +6,29 @@ const cors = require("cors");
 const blogData = require("./public/db/blogData.json");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-
 app.get("/", (req, res) => {
   res.render("index", { blogs: blogData });
 });
 
-app.get("/:blogName", (req, res) => {
-    const requestedBlogName = req.params.blogName;
-    let foundBlog = null;
-  
-    // Find the blog in blogData based on the requested name
-    for (const blog of blogData) {
-      if (blog.name === requestedBlogName) {
-        foundBlog = blog;
-        break;
-      }
-    }
-  
-    if (foundBlog) {
-      // Render the "blog" view and pass the foundBlog data to it
-      res.render("./blog", { blog: foundBlog });
+app.get("/blog/:blogName", (req, res) => {
+  let flag = 1;
+  for (let i = 0; i < blogData.length; i++) {
+    if (blogData[i].name === req.params.blogName) {
+      flag = 1;
+      res.status(200).render("blog", { blog: blogData[i] });
+      //   res.status(200).send({ blogData });
+      return;
     } else {
-      res.status(404).send("Error 404: Not Found");
+      flag = 0;
     }
-  });
-  
+  }
+  if (flag === 0) {
+    res.send("Error 404: Not Found ");
+  }
+});
 
 app.get("*", (req, res) => {
   res.render("partials/damn");
@@ -42,4 +37,4 @@ app.get("*", (req, res) => {
 const PORT = 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); 
+});
