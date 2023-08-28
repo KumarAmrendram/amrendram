e8f741ed1f42ccf134eee442685f554c82c881a9
kumarAmrendram <amrendram07@gmail.com>
2023-08-28 10:54:45 +0530

    - lowercase blog.name

--- app.js
+++ app.js
@@ -6,35 +6,38 @@ const cors = require("cors");
 const blogData = require("./public/db/blogData.json");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
+
 app.get("/", (req, res) => {
-    res.render("index", { blogs: blogData });
+  res.render("index", { blogs: blogData });
 });
 
 app.get("/blog/:blogName", (req, res) => {
-    let flag = 1;
-    for (let i = 0; i < blogData.length; i++) {
-        if (blogData[i].name === req.params.blogName) {
-            flag = 1;
-            res.status(200).render("blog.ejs", { blog: blogData[i] });
-            //   res.status(200).send({ blogData });
-            return;
-        } else {
-            flag = 0;
-        }
+  const requestedBlogName = req.params.blogName.toLowerCase(); // Convert to lowercase
+  let foundBlog = null;
+
+  for (let i = 0; i < blogData.length; i++) {
+    const blogName = blogData[i].name.toLowerCase(); // Convert to lowercase
+    if (blogName === requestedBlogName) {
+      foundBlog = blogData[i];
+      break; // Exit the loop as soon as a match is found
     }
-    if (flag === 0) {
-        res.send("Error 404: Not Found ");
-    }
+  }
+
+  if (foundBlog) {
+    res.status(200).render("blog.ejs", { blog: foundBlog });
+  } else {
+    res.send("Error 404: Not Found ");
+  }
 });
 
-app.get('*', (req, res) => {
-    res.render("partials/damn")
-})
+app.get("*", (req, res) => {
+  res.render("partials/damn");
+});
 
 const PORT = 7000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
