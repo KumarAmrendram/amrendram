943aa68725b482d29b51cdf683e71f05b3d96b05
kumarAmrendram <amrendram07@gmail.com>
2023-08-29 17:20:56 +0530

    - removes bootstrap

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
+
+//routes 
+
+// home route 
+
+app.get("/", (req, res)=> {
+  res.render("index")
+})
 app.get("/blogs", (req, res) => {
   res.render("partials/blogs", { blogs: blogData });
 });
